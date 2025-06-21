9936d779c6f51fee1c0a642f1101240373e03589
akankshag12345 <guravakanksha43@gmail.com>
2025-06-21 14:52:55 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDgBupZJpPBfR8J5Hb5XteBkaKrLgWv8yg",
+  authDomain: "login-authentication-9d64a.firebaseapp.com",
+  projectId: "login-authentication-9d64a",
+  storageBucket: "login-authentication-9d64a.firebasestorage.app",
+  messagingSenderId: "815574919806",
+  appId: "1:815574919806:web:b04eb3fe2db8cba394862f"
+};
